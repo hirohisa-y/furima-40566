93244c7bf9df0e1368c15727164f8b01d78a2c7a
hirohisa-y <117285662+hirohisa-y@users.noreply.github.com>
2024-07-21 16:16:15 +0900

    ユーザー管理機能の修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
       it "nicknameと,last_name,first_name,last_name_kana,first_name_kana,date_of_birth,email,passwordとpassword_confirmationが存在すれば登録できる" do
+        expect(@user).to be_valid
       end
     end
 
@@ -42,6 +43,13 @@ RSpec.describe User, type: :model do
         @user.password_confirmation = @user.password
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
+      end
+
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
 
       it '重複したemailが存在する場合は登録できない' do
@@ -81,11 +89,24 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("First name kana can't be blank")
       end
 
+      it 'last_name_kanaにカタカナ以外の文字が含まれている場合は登録できない' do
+        @user.last_name_kana = 'あ亜'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana is invalid. Input full-width katakana characters.")
+      end
+
+
+      it 'first_name_kanaにカタカナ以外の文字が含まれている場合は登録できない' do
+        @user.first_name_kana = 'あ亜'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid. Input full-width katakana characters.")
+      end
+
       it 'date_of_birthが空では登録できない' do
         @user.date_of_birth = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Date of birth can't be blank")
-      end
+      end  
 
       it 'last_nameが半角では登録できない' do
         @user.last_name = 'abc'
