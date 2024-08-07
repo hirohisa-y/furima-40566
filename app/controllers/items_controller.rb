862433fd51a25d70cd2d5d90de2411599af60a11
hirohisa-y <117285662+hirohisa-y@users.noreply.github.com>
2024-08-07 15:16:11 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :number_of_day_id, :price, :image).merge(user_id: current_user.id)
